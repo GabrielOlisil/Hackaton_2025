a6c493149e1b8cdbabb559a6825093ce20095917
Gabriel de Oliveira Silva <gabriel.oliveira.s177@gmail.com>
2025-07-21 17:38:37 -0400

    add sys

--- places/apps.py
+++ places/apps.py
@@ -1,5 +1,5 @@
 from django.apps import AppConfig
-
+import sys
 
 
 class PlacesConfig(AppConfig):
@@ -7,4 +7,20 @@ class PlacesConfig(AppConfig):
     name = 'places'
     def ready(self):
         from . import scheduler
-        scheduler.start()
+        management_commands = [
+            'makemigrations',
+            'migrate',
+            'collectstatic',
+            'shell',
+            'createsuperuser'
+        ]
+
+        # Verifica se algum dos comandos acima está sendo executado.
+        is_management_command = any(
+            cmd in sys.argv for cmd in management_commands
+        )
+
+        # Inicia o agendador apenas se NÃO for um comando de gerenciamento.
+        if not is_management_command:
+            from . import scheduler
+            scheduler.start()
